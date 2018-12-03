63d86262297a62a040f943dd83955b4fe7b0e6ed
a0tempest <44822417+a0tempest@users.noreply.github.com>
2018-12-03 03:34:56 -0500

    Update ad.ps1

--- ad.ps1
+++ ad.ps1
@@ -7,8 +7,6 @@ Import-Module ADDSDeployment
 # Making it a DC
 Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath “C:\Windows\NTDS” -DomainMode “7” -DomainName “young.local” -DomainNetbiosName “YOUNG” -ForestMode “7” -InstallDns:$true -LogPath “C:\Windows\NTDS” -NoRebootOnCompletion:$true -SysvolPath “C:\Windows\SYSVOL” -Force:$true -SafeModeAdministratorPassword (ConvertTo-SecureString "P@ssword!" -AsPlainText -Force)
 
-# Renaming Computer 
-Rename-Computer -NewName ad01-young
 
 # Restarting Computer
 Restart-Computer -Force

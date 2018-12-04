781034c3de6005d10c39109d5b2e4797101449f8
a0tempest <44822417+a0tempest@users.noreply.github.com>
2018-12-04 00:08:23 -0500

    Update transfer.bat

--- transfer.bat
+++ transfer.bat
@@ -1,6 +1,12 @@
 @echo off 
-echo Transfering Files to Server
-pscp.exe -pw Ch@mpl@1n!18 C:\Users\young.chen\Desktop\wp.sh root@10.0.5.20:/
-pscp.exe -pw Ch@mpl@1n!18 C:\Users\young.chen\Desktop\sshd_config root@10.0.5.20:/
-pscp.exe -pw Ch@mpl@1n!18 C:\Users\young.chen\Desktop\wp-config.php root@10.0.5.20:/
+echo Transfering Files to Blog Server
+pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\blog.sh root@10.0.5.20:/
+pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\sshd_config root@10.0.5.20:/
+pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\wp-config.php root@10.0.5.20:/
+
+echo Transfering Files to DHCP Server
+pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\dhcp.sh root@10.0.5.20:/
+pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\sshd_config root@10.0.5.20:/
+pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\dhcpd.conf root@10.0.5.20:/
+
 echo Done!!

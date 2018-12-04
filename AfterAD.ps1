01ace933be50a3a9d0b56930f7834760e7e6291c
a0tempest <44822417+a0tempest@users.noreply.github.com>
2018-12-04 01:24:18 -0500

    Add files via upload

--- AfterAD.ps1
+++ AfterAD.ps1
@@ -20,5 +20,18 @@ Add-DnsServerResourceRecordA -Name "lin01-young" -IPv4Address "10.0.5.33" -ZoneN
 # Renaming Computer 
 Rename-Computer -NewName ad01-young
 
-# Restarting Computer
-Restart-Computer -Force
+# Tranferring files to Blog server 
+.\pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\blog.sh root@10.0.5.20:/
+.\pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\sshd_config root@10.0.5.20:/
+.\pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\wp-config.php root@10.0.5.20:/
+
+# Transferring Files to DHCP Server
+.\pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\dhcp.sh root@10.0.5.33:/
+.\pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\sshd_config root@10.0.5.33:/
+.\pscp.exe -pw Ch@mpl@1n!18 C:\Users\Administrator\Desktop\dhcpd.conf root@10.0.5.33:/
+
+# Running blog.sh
+.\putty.exe -ssh root@10.0.5.20 -pw Ch@mpl@1n!18 -m .\cblog.txt
+
+#  Running dhcp.sh
+.\putty.exe -ssh root@10.0.5.33 -pw Ch@mpl@1n!18 -m .\cdhcp.txt

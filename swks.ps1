ab0f948ffb7a39dcd6402c466cbcd03de72795a5
a0tempest <44822417+a0tempest@users.noreply.github.com>
2018-12-05 21:45:07 -0500

    Add files via upload

---
+++ swks.ps1
@@ -0,0 +1,9 @@
+﻿# Allow remote Scipts 
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force -Verbose
+
+# Adding computer to domain
+$domain = "young.local"
+$username = "young.chen-adm"
+$password = "P@ssword!" | ConvertTo-SecureString -AsPlainText -Force
+$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username,$password
+Add-Computer -DomainName $domain -Credential $creds -Restart -Verbose
